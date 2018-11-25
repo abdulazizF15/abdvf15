84aa1f6e0a39885a4e14c7d9432991c6f9c29a1d
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2018-11-26 01:57:05 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+    client.user.setGame(` ع `,'https://www.twitch.tv/M')
+
+});
+client.login(process.env.BOT_TOKEN);

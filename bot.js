0188ee47bfed4278d5a48ec30bf4ac52df80ee7a
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2018-11-26 02:19:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(` انني مدير كبير يا احمق `,'https://www.twitch.tv/M')
+    client.user.setGame(`  ⠉̮⃝✌️ `,'https://www.twitch.tv/M')
 
 });
 client.login(process.env.BOT_TOKEN);

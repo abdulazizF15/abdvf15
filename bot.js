aaede3d4180636f79b59abda22d4e5724ab2ed4b
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2018-12-01 01:51:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`  ‏سوف املئ كأسي و اشرب شتبي انت ؟. `,'https://www.twitch.tv/M')
+    client.user.setGame(`  ‏‏من الان وصاعدًا جميع الأمور على زق . `,'https://www.twitch.tv/M')
 
 });
 client.login(process.env.BOT_TOKEN);

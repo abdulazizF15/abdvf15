9dd517f56e06d18ca51db421ac554d34f435414b
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2018-11-30 20:20:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`  m7Md#8188 . `,'https://www.twitch.tv/M')
+    client.user.setGame(`  ‏ياشين اللي يحسبك خاق عليه وانت خاق عليه صدق . `,'https://www.twitch.tv/M')
 
 });
 client.login(process.env.BOT_TOKEN);

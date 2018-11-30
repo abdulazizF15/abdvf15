420dc28815058a0957382a8187ea331903f0ffc5
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2018-11-30 18:58:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`  بأمكاني تدمير هذا العالم بس امي قالت مانبي مشاكل . `,'https://www.twitch.tv/M')
+    client.user.setGame(`  m7Md#8188 . `,'https://www.twitch.tv/M')
 
 });
 client.login(process.env.BOT_TOKEN);

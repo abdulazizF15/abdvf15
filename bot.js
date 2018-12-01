68242a67c8ad20950b545896774006637057fc16
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2018-12-01 14:34:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`  ‏‏MsharE . `,'https://www.twitch.tv/M')
+    client.user.setGame(`  ‏‏MShArE . `,'https://www.twitch.tv/M')
 
 });
 client.login(process.env.BOT_TOKEN);

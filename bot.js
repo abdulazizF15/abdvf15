ac8687c92162a4e812dadda5ca84443055ff8423
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2018-12-03 00:34:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`  ‏‏MShArE . `,'https://www.twitch.tv/M')
+    client.user.setGame(`  ‏‏Maybe, i’m just a stranger in this fucking world.. `,'https://www.twitch.tv/M')
 
 });
 client.login(process.env.BOT_TOKEN);

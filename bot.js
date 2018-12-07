605d39675fe213f8414814bf62ae1d36784bfed1
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2018-12-07 15:23:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(` ' `,'https://www.twitch.tv/M')
+    client.user.setGame(` [:ما راح تلاقون مثلي لا ازعل ولا اتغلى وصاحي ليل نهار وارد بسرعه وفوق كذا مسحوب علي لكن من اهمل النعمة يجاري بفقدها `,'https://www.twitch.tv/M')
 
 });
 client.login(process.env.BOT_TOKEN);

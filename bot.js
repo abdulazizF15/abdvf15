e054e3d72a26831f83d3fddf4ed02895c16463e3
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2018-12-08 08:41:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(` Nothing `,'https://www.twitch.tv/M')
+    client.user.setGame(` nothing. `,'https://www.twitch.tv/g2k')
 
 });
 client.login(process.env.BOT_TOKEN);

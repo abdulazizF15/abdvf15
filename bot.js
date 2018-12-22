998e64fb582c91fdc7cdf09f1d5305fc74ab63d9
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2018-12-22 05:08:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,15 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-client.on('ready', () => {
-    client.user.setGame(` - `,'https://www.twitch.tv/k')
-
+const D1 = require("discord.js");
+const D2 = require("discord.js");
+//////////////////////////////////////////////////
+const abdulaziz = new D1.Client();
+const a7q9 = new D2.Client();
+/////////////////////////////////////////////////
+abdulaziz.on('ready', () => {
+abdulaziz.user.setGame(` - `,'https://www.twitch.tv/1')
 });
-client.login(process.env.BOT_TOKEN);
+a7q9.on('ready', () => {
+a7q9.user.setGame(` لا احد يهتم. `,'https://www.twitch.tv/a7q9')
+});
+/////////////////////////////////////////////////
+abdulaziz.login(process.env.AZIZ);
+a7q9.login(process.env.A7Q9);

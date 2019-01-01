8243798c32f0cd79ac59106a0dd8e8e0804e95ac
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2019-01-01 08:22:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(` - `,'https://www.twitch.tv/tfue')
+    client.user.setGame(` انتهت مصلحتهم فلم نعد نراهم 🙂. `,'https://www.twitch.tv/tfue')
 
 });
 client.login(process.env.BOT_TOKEN);

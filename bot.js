9a073a4f76b9ec72f4d74cf197d07264065a318f
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2019-02-03 17:12:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('ready', () => {
-    client.user.setGame(` Vendetta. `,'https://www.twitch.tv/IDK')
 
+client.on('ready', () => {
+  client.user.setActivity(" Vendetta. ",{type: 'WATCHING'})
+  client.user.setStatus("dnd")
 });
+
 client.login(process.env.BOT_TOKEN);

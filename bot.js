7a0a22c8878a58031ac1d79a950256db1a13656a
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2019-02-03 18:12:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,38 @@
-///////////////////////////////////////////////////////////
-
+```
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const developers = ['312075421992615946'];
 
-///////////////////////////////////////////////////////////
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(` M. `,'https://www.twitch.tv/IDK')
   client.user.setStatus("idle")
 });
 
-///////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
 
+if (message.content.startsWith('Abdulazizwt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+  client.user.setStatus("idle")
+    message.channel.send(`Done.`)
+} else 
+if (message.content.startsWith('Abdulazizls')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+  client.user.setStatus("idle")
+    message.channel.send(`Done.`)
+} else 
+if (message.content.startsWith('Abdulazizpl')) {
+client.user.setActivity(argresult , {type:'PLAYING'});
+  client.user.setStatus("idle")
+    message.channel.send(`Done.`)
+} else 
+if (message.content.startsWith('Abdulazizst')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/IDK");
+  client.user.setStatus("idle")
+   message.channel.send(`Done.`)
+}
+});
 client.login(process.env.BOT_TOKEN);
+```

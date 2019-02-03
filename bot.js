4dbc8aa64d3743e6d7b0341ee165bf8b5876650d
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2019-02-03 19:02:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,36 @@
 const Discord = require('discord.js');
-const abdulaziz = new Discord.abdulaziz();
+const client = new Discord.Client();
 const developers = ['312075421992615946'];
 
 
-abdulaziz.on('ready', () => {
-  console.log(`Logged in as ${abdulaziz.user.tag}!`);
-  abdulaziz.user.setStatus("idle")
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setStatus("idle")
 });
 
-abdulaziz.on('message', message => {
+client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!developers.includes(message.author.id)) return;
 
 if (message.content.startsWith('Mwt')) {
-abdulaziz.user.setActivity(argresult, {type:'WATCHING'});
-  abdulaziz.user.setStatus("idle")
+client.user.setActivity(argresult, {type:'WATCHING'});
+  client.user.setStatus("idle")
     message.channel.send(`Done.`)
 } else 
 if (message.content.startsWith('Mls')) {
-abdulaziz.user.setActivity(argresult , {type:'LISTENING'});
-  abdulaziz.user.setStatus("idle")
+client.user.setActivity(argresult , {type:'LISTENING'});
+  client.user.setStatus("idle")
     message.channel.send(`Done.`)
 } else 
 if (message.content.startsWith('Mpl')) {
-abdulaziz.user.setActivity(argresult , {type:'PLAYING'});
-  abdulaziz.user.setStatus("idle")
+client.user.setActivity(argresult , {type:'PLAYING'});
+  client.user.setStatus("idle")
     message.channel.send(`Done.`)
 } else 
 if (message.content.startsWith('Mst')) {
-  abdulaziz.user.setGame(argresult, "https://www.twitch.tv/IDK");
-  abdulaziz.user.setStatus("idle")
+  client.user.setGame(argresult, "https://www.twitch.tv/IDK");
+  client.user.setStatus("idle")
    message.channel.send(`Done.`)
 }
 });
-abdulaziz.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);

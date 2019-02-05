0e543c538bc6848f7712953f78d9b55304365af7
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2019-02-05 15:23:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,36 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('ready', () => {
-    client.user.setGame(` Vendetta. `,'https://www.twitch.tv/IDK')
+const developers = ['312075421992615946'];
 
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setStatus("idle")
+});
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
+
+if (message.content.startsWith('Mwt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+  client.user.setStatus("idle")
+    message.channel.send(`Done.`)
+} else 
+if (message.content.startsWith('Mls')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+  client.user.setStatus("idle")
+    message.channel.send(`Done.`)
+} else 
+if (message.content.startsWith('Mpl')) {
+client.user.setActivity(argresult , {type:'PLAYING'});
+  client.user.setStatus("idle")
+    message.channel.send(`Done.`)
+} else 
+if (message.content.startsWith('Mst')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/IDK");
+  client.user.setStatus("idle")
+   message.channel.send(`Done.`)
+}
 });
 client.login(process.env.BOT_TOKEN);

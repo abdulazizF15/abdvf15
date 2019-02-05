bc6904d4e475ee24442924eae5099cff5d46d0a2
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2019-02-05 14:38:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity(" كل شي(ن) صار في ساعة غضب . ",{type: 'LISTENING'})
+    client.user.setActivity(" لاحياة للحب بعد موت الاهتمام . ",{type: 'LISTENING'})
     client.user.setStatus("idle")
 
 });

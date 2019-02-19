3ed808415b22c02b517e38b2f3cae4e9d5b537a3
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2019-02-19 21:25:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,15 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-client.on('ready', () => {
-    client.user.setGame(` Vendetta. `,'https://www.twitch.tv/IDK')
-
+const D1 = require("discord.js");
+const D2 = require("discord.js");
+//////////////////////////////////////////////////
+const abdulaziz = new D1.Client();
+const a7q9 = new D2.Client();
+/////////////////////////////////////////////////
+abdulaziz.on('ready', () => {
+abdulaziz.user.setGame(` Vendetta. `,'https://www.twitch.tv/idk')
 });
-client.login(process.env.BOT_TOKEN);
+a7q9.on('ready', () => {
+a7q9.user.setGame(` You. `,'https://www.twitch.tv/idk')
+});
+/////////////////////////////////////////////////
+abdulaziz.login(process.env.AZIZ);
+a7q9.login(process.env.A7Q9);

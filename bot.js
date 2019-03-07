2dc32f233041f89669fb2a245f66caff72e96cee
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2019-03-07 21:52:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(` .علئ شباك الأمل زقت عصافيري `,'https://www.twitch.tv/IDK')
+    client.user.setGame(` SHUT
+UP. `,'https://www.twitch.tv/IDK')
 
 });
 client.login(process.env.AZIZ);

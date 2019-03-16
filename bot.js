6fc7758b3e7b6d37bb54f5545ae903dd2c7a0fba
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2019-03-16 22:27:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(` SHUT
-UP. `,'https://www.twitch.tv/IDK')
+    client.user.setGame(` SHUT UP. `,'https://www.twitch.tv/IDK')
 
 });
 client.login(process.env.AZIZ);
